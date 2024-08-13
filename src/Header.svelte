efdc116f89a9bbcebfd32d7f028ff542c00aa32c
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-13 14:55:51 +0200

    Add wishlist

--- src/Header.svelte
+++ src/Header.svelte
@@ -41,3 +41,35 @@
             />
           </svg>
         </button>
+
+        <div class={`w-full md:block md:w-auto ${showNavbar ? '' : 'hidden'}`} id="navbar-dropdown">
+          <ul class="flex flex-col mt-4 border border-gray-100 rounded-lg bg-gray-500 md:space-x-8 md:flex-row md:mt-0 md:border-0 p-4 md:p-0">
+            <li>
+              <Link
+                to="/wishlist"
+                class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700"
+              >
+                Wishlist
+              </Link>
+            </li>
+            <li class="relative hidden md:block lg:block">
+              <div class="absolute left-3 top-0">
+                <p class="flex h-2 w-2 items-center justify-center rounded-full bg-red-500 p-3 text-xs text-white">2</p>
+              </div>
+              <Link to="/cart" class="text-white hover:bg-gray-700">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-6 w-6 stroke-white cursor-pointer"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                  />
+                </svg>
+              </Link>
+            </li>

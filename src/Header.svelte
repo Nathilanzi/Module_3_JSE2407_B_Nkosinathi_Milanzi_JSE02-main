cdc7de305a9a21d573e15ddce8418a02430aa98f
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-13 14:56:46 +0200

    Add Login

--- src/Header.svelte
+++ src/Header.svelte
@@ -79,3 +79,15 @@
               </Link>
             </li>
             <li>
+              <Link
+                to="/login"
+                class="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700"
+              >
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>

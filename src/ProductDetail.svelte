44ff4abfe2e33191257496e3e9564e919715aff8
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-13 14:48:24 +0200

    Add import statements and consts

---
+++ src/ProductDetail.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { onMount } from 'svelte';
+  import { Link } from 'svelte-routing';
+
+  export let params = {}; // Route parameters passed from the parent component
+  let product = null;
+  let productId = params.id || ''; // Extract productId from params
+
+  const fetchProduct = async () => {
+    try {
+      const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
+      if (res.ok) {
+        product = await res.json();
+      } else {
+        console.error('Failed to fetch product');
+      }
+    } catch (error) {
+      console.error('Error fetching product:', error);
+    }
+  };
+
+  onMount(() => {
+    fetchProduct();
+  });
+</script>

0961359226437ec35433447e6aa6d8a2b5bc89cf
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-13 14:50:14 +0200

    Add styles

--- src/ProductList.svelte
+++ src/ProductList.svelte
@@ -28,3 +28,48 @@
     
     fetchCategories();
     </script>
+
+    <style>
+       .product-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 1rem;
+  }
+      .product-card {
+        border: 1px solid #090808;
+        padding: 1rem;
+        border-radius: 8px;
+        cursor: pointer;
+        width: 200px;
+        background-color: white;
+      }
+    </style>
+    
+    <select on:change={handleCategoryChange}>
+      <option value=''>All Categories</option>
+      {#each categories as category}
+        <option value={category}>{category}</option>
+      {/each}
+    </select>
+    
+    <select on:change={handleSortChange}>
+      <option value=''>Default</option>
+      <option value='low-to-high'>Price: Low to High</option>
+      <option value='high-to-low'>Price: High to Low</option>
+    </select>
+    
+    <div class="product-list">
+      {#each filteredProducts as product}
+        <Link to="/product/{product.id}">
+          <div class="product-card">
+            <img src={product.image} alt={product.title} class="w-40 max-w-[300px] h-auto object-contain rounded-lg" />
+            <h2>{product.title}</h2>
+            <p>${product.price}</p>
+            <p>Category: {product.category}</p>
+            <p>Ratings: {product.rating.rate}</p>
+            <p>Reviews: {product.rating.count}</p>
+          </div>
+        </Link>
+      {/each}
+    </div>
+    

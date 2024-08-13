a272c7095897cc35d264873f64c064501006cf18
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-08-13 14:52:08 +0200

    Add scripts

---
+++ src/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+    // @ts-ignore
+    import { Link } from 'svelte-routing';
+    let showNavbar = false;
+  
+    const toggleNavbar = () => {
+      showNavbar = !showNavbar;
+    };
+  </script>
